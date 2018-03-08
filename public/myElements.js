de0a85b95e56387520d795a6eac31854db748026
millesabords <703804+millesabords@users.noreply.github.com>
2018-03-08 13:39:28 +0100

    first version ready

--- public/myElements.js
+++ public/myElements.js
@@ -16,6 +16,78 @@ function copyTextToClipBoard() {
     }
 }
 
+//synchronize menu item highlighting with active section
+//todo: make this work: it seems like the this.scrolltop or this.offset.top are not properly set
+/*$(document).ready(function(){
+  var $sections = $('.mainSections');
+  $(window).scroll(function(){
+    var currentScroll = $(this).scrollTop();
+    var $currentSection = $(this);
+    $sections.each(function(){
+      var divPosition = $(this).offset().top;
+      if( divPosition - 1 < currentScroll ){
+        $currentSection = $(this);
+      var id = $currentSection.attr('id');
+//   	 $('a').removeClass('sectionActive');
+        //$('[href=#' + id + ']').addClass('sectionActive');
+			//or add attribute pseudo selector 'active'?
+//        $('#id_' + id).addClass('sectionActive');
+			console.log("currentSection id = " + id);
+      }
+    })
+  });
+});
+*/
+
+//another one, does not seem to work all the time
+$(document).ready(function () {
+    $(document).on("scroll", onScroll);
+    
+    //smoothscroll
+    $('a[href^="#"]').on('click', function (e) {
+        e.preventDefault();
+        $(document).off("scroll");
+        
+        $('a').each(function () {
+            $(this).removeClass('active');
+        })
+        $(this).addClass('active');
+      
+        var target = this.hash,
+            menu = target;
+        $target = $(target);
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top+2
+        }, 500, 'swing', function () {
+            window.location.hash = target;
+            $(document).on("scroll", onScroll);
+        });
+    });
+});
+function onScroll(event){
+    var scrollPos = $(document).scrollTop();
+  var $sections = $('.mainSections');
+    $sections.each(function(){
+    //$('#menu-center a').each(function () {
+        var currLink = $(this);
+        var refElement = $(currLink.attr("href"));
+		var refElementPos = refElement.position();
+	if(typeof refElementPos !== 'undefined' &&
+		typeof refElementPos.top !== 'undefined' &&
+		refElement.position().top <= scrollPos &&
+		refElement.position().top + refElement.height() > scrollPos) {
+            //$('#menu-center ul li a').removeClass("active");
+		//$('#navbarCollapse li').removeClass('active');
+    		window.location.hash = "";//#" + hash;
+            currLink.addClass("active");
+        }
+        else{
+            currLink.removeClass("active");
+        }
+    });
+}
+
+/*form method called before validation sent to server*/
 function cvalidateForm(){
    let vname = document.forms["contactForm"]["mailerName"].value,
    	vmail = document.forms["contactForm"]["mailerMail"].value,
@@ -29,9 +101,9 @@ function cvalidateForm(){
    return true;
 }
 
-function scrollTo(hash) {
+ /*function scrollTo(hash) {
     location.hash = "#" + hash;
-}
+}*/
 
 /**
  * animated href scroll
@@ -39,6 +111,7 @@ function scrollTo(hash) {
  * If href argument ommited, assumes context (this) is HTML Element,
  * which will be the case when invoked by jQuery after an event
  */
+/*
 function scroll_if_anchor(href) {
     href = typeof(href) == "string" ? href : $(this).attr("href");
     
@@ -60,14 +133,15 @@ function scroll_if_anchor(href) {
             }
         }
     }
-}    
+}   
 // When our page loads, check to see if it contains and anchor
 scroll_if_anchor(window.location.hash);
 // Intercept all anchor clicks
 $("body").on("click", "a", scroll_if_anchor);
+*/
 
+//todo: finish with feedback message when form has been succesfully submitted (or not)
 //var formProcessed = "{{formProcessed}}";
-
 //if(typeof formProcessed === 'undefined'){
 console.log("value of formProcessed=" + formProcessed + $("formProcessed")); 
 if(formProcessed === "true"){
@@ -78,3 +152,7 @@ if(formProcessed === "true"){
 }
 
 
+/*$('.selector').click(function(e){*/
+    /*$(this).toggleClass('new-class');*/
+    /*e.preventDefault();*/
+/*});*/
